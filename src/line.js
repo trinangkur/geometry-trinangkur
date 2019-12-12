bba9bfb83fd72b79ed7ad531b6d9340bd276e39a
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-12 16:04:40 +0530

    modified isEqual
    modified is equal to make it more generic...

--- src/line.js
+++ src/line.js
@@ -9,12 +9,12 @@ class Line {
     return `Line {x1: ${this.x1}, y1: ${this.y1}, x2: ${this.x2}, y2: ${this.y2}}`;
   }
   isEqual(other) {
-    return (
-      this.x1 == other.x1 &&
-      this.y1 == other.y1 &&
-      this.x2 == other.x2 &&
-      this.y2 == other.y2
-    );
+    for (let key in other) {
+      if (other[key] != this[key]) {
+        return false;
+      }
+      return true;
+    }
   }
 }
 
