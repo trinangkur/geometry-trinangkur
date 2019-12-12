0d21dc147c285dfd5e17b97fa30421e734f84bf1
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-12 15:56:59 +0530

    implemented isEqual method inside Line
    Implemented a isEqual method inside Line to check if all the elements
    of given object is same as the object created by class...

--- src/line.js
+++ src/line.js
@@ -8,6 +8,14 @@ class Line {
   toString() {
     return `Line {x1: ${this.x1}, y1: ${this.y1}, x2: ${this.x2}, y2: ${this.y2}}`;
   }
+  isEqual(other) {
+    return (
+      this.x1 == other.x1 &&
+      this.y1 == other.y1 &&
+      this.x2 == other.x2 &&
+      this.y2 == other.y2
+    );
+  }
 }
 
 module.exports = { Line };
--- test/testLine.js
+++ test/testLine.js
@@ -7,6 +7,22 @@ describe("line", function() {
       actual = actual.toString();
       let expected = "Line {x1: 0, y1: 0, x2: 2, y2: 0}";
       assert.deepStrictEqual(actual, expected);
+      actual = new Line(0, 3, 4, 0);
+      actual = actual.toString();
+      expected = "Line {x1: 0, y1: 3, x2: 4, y2: 0}";
+      assert.deepStrictEqual(actual, expected);
+    });
+  });
+  describe("isEqual", function() {
+    it("should validate if properties of given line object is equal to its self", function() {
+      let actual = new Line(0, 0, 1, 3);
+      actual = actual.isEqual({ x1: 0, y1: 0, x2: 1, y2: 3 });
+      assert.strictEqual(actual, true);
+    });
+    it("should not validate if properties of given line object is not equal to its self", function() {
+      let actual = new Line(1, 0, 1, 3);
+      actual = actual.isEqual({ x1: 0, y1: 0, x2: 1, y2: 3 });
+      assert.strictEqual(actual, false);
     });
   });
 });
