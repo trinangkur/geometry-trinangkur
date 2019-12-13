792b19cd23c9b558c5a85565ccf531dee862a4b3
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-13 15:55:19 +0530

    Modified the constructor of class Line...
    now giving values instead of reference inside object...

--- src/line.js
+++ src/line.js
@@ -4,11 +4,14 @@ const arePointsEqual = function(point1, point2) {
 
 class Line {
   constructor(endA, endB) {
-    [this.endA, this.endB] = [endA, endB];
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
+
   toString() {
     return `Line (${this.endA.x}, ${this.endA.y})(${this.endB.x}, ${this.endB.y})`;
   }
+
   isEqual(other) {
     return (
       other instanceof Line &&
