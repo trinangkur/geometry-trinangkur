8ef10398ad53c5227c49c530553480cc2a52982b
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-16 15:02:08 +0530

    Added new class circle and implemented toString
    added test for toString as well..

--- test/testCircle.js
+++ test/testCircle.js
@@ -3,7 +3,7 @@ const assert = require("chai").assert;
 
 describe("Circle", function() {
   describe("toString", function() {
-    it("should give string representation of given circle", function() {
+    it("should give string representation of given circle ", function() {
       const circle = new Circle({ x: 1, y: 2 }, 5);
       assert.strictEqual(circle.toString(), "[Circle @(1,2) radius 5]");
     });
