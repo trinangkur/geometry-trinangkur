b7d38b84524b4ae0dce743af241cfaf0c0f994e1
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-17 15:02:13 +0530

    Added new property area of Rectangle
    to get the area calculating width and length and added them as property
    as well

--- src/rectangle.js
+++ src/rectangle.js
@@ -3,25 +3,37 @@ const Point = require("../src/point");
 
 class Rectangle {
   constructor(endA, endC) {
-    this.edgeA = new Point(endA.x, endA.y);
-    this.edgeB = new Point(endC.x, endA.y);
-    this.edgeC = new Point(endC.x, endC.y);
-    this.edgeD = new Point(endA.x, endC.y);
-
+    this.vertexA = new Point(endA.x, endA.y);
+    this.vertexB = new Point(endC.x, endA.y);
+    this.vertexC = new Point(endC.x, endC.y);
+    this.vertexD = new Point(endA.x, endC.y);
     this.diagonal = new Line(endA, endC);
+
     Object.defineProperties(this, {
       diagonal: { writable: false },
-      edgeA: { enumerable: true, writable: false },
-      edgeB: { enumerable: true, writable: false },
-      edgeC: { enumerable: true, writable: false },
-      edgeD: { enumerable: true, writable: false }
+      vertexA: { enumerable: true, writable: false },
+      vertexB: { enumerable: true, writable: false },
+      vertexC: { enumerable: true, writable: false },
+      vertexD: { enumerable: true, writable: false }
     });
+  }
+
+  get lenght() {
+    return this.vertexA.findDistanceTo(this.vertexB);
+  }
+
+  get width() {
+    return this.vertexA.findDistanceTo(this.vertexD);
   }
 
   toString() {
     const endA = `(${this.diagonal.endA.x},${this.diagonal.endA.y})`;
     const endB = `(${this.diagonal.endB.x},${this.diagonal.endB.y})`;
     return `[Rectangle ${endA} to ${endB}]`;
+  }
+
+  get area() {
+    return this.lenght * this.width;
   }
 }
 
--- test/testRectangle.js
+++ test/testRectangle.js
@@ -8,4 +8,18 @@ describe("rectange", function() {
       assert.strictEqual(rectangle.toString(), "[Rectangle (1,1) to (2,3)]");
     });
   });
+  describe("area", function() {
+    it("should get area of a given rectangle of inclined diagonal", function() {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 2, y: 3 });
+      assert.strictEqual(rectangle.area, 2);
+    });
+    it("should get area of given diagonal paralle to x-axis", function() {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 1 });
+      assert.strictEqual(rectangle.area, 0);
+    });
+    it("should get area of given diagonal paralle to y-axis", function() {
+      const rectangle = new Rectangle({ x: 1, y: 8 }, { x: 1, y: 1 });
+      assert.strictEqual(rectangle.area, 0);
+    });
+  });
 });
